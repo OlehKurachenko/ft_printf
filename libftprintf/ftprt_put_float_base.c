0e2258719aa6ac6f8f3a660ae8269369897d68a6
okurache <okurache@student.unit.ua>
2018-01-18 16:22:44 +0200

    configuring put_f

--- libftprintf/ftprt_put_float_base.c
+++ libftprintf/ftprt_put_float_base.c
@@ -41,6 +41,27 @@ static void		put_prepoint_part(long double *const val, const long double base,
 	}
 }
 
+static void		put_prepoint_part_apo(long double *const val, const long double base,
+									 t_printff *const fl, t_putchar f_putchar)
+{
+	const unsigned char	is_capital = (unsigned char)(fl->len_flag == 22);
+	long double			downstep;
+
+	// TODO configure apo
+	downstep = 1l;
+	while (*val / downstep >= base)
+		downstep *= base;
+	fl->flags[4] = 1;
+	while (*val >= 1l || fl->flags[4])
+	{
+		f_putchar(ftprt_getupdecimal((unsigned char)(*val / downstep),
+									 is_capital));
+		*val -= (uintmax_t)(*val/downstep);
+		downstep /= base;
+		fl->flags[4] = 0;
+	}
+}
+
 /*
 **	Here, flag[6] will be used to show that number is < 0
 */
@@ -49,6 +70,9 @@ void			ftprt_put_float_base(long double val, const long double base,
 	t_printff *const fl, t_putchar f_putchar)
 {
 	val += 0.5l * ft_ldpow(1l / base, (size_t)fl->precision);
-	put_prepoint_part(&val, base, fl, f_putchar);
+	if (fl->flags[4])
+		put_prepoint_part_apo(&val, base, fl, f_putchar);
+	else
+		put_prepoint_part(&val, base, fl, f_putchar);
 	put_afterpoint_part(val, base, fl, f_putchar);
 }
