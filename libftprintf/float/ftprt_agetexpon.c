17d5e5b40ee791c2d3e7ddff576fafe534ce3fde
okurache <okurache@student.unit.ua>
2018-01-28 15:04:59 +0200

    ftprt_agetexpon fixed

--- libftprintf/float/ftprt_agetexpon.c
+++ libftprintf/float/ftprt_agetexpon.c
@@ -2,7 +2,7 @@
 
 static const size_t 	infinite_loop_number = 1000;
 
-static size_t			normalize_val(int *const res,
+static size_t			normalize_val(t_printff *const fl, int *const res,
 								   long double *const val)
 {
 	size_t	i;
@@ -19,6 +19,36 @@ static size_t			normalize_val(int *const res,
 		*val *= 2l;
 		--(*res);
 	}
+	while ((*val += 0.5l * ft_ldpow(1l / 16l, (size_t)fl->precision)) >= 16l)
+	{
+		*val /= 2l;
+		++(*res);
+	}
+	return (i);
+}
+
+static size_t			normalize_val_sh(t_printff *const fl, int *const res,
+									   long double *const val)
+{
+	size_t	i;
+
+	*res = 0;
+	while (*val >= 2l)
+	{
+		*val /= 2l;
+		++(*res);
+	}
+	i = 0;
+	while (*val < 1l && ++i < infinite_loop_number)
+	{
+		*val *= 2l;
+		--(*res);
+	}
+	while ((*val += 0.5l * ft_ldpow(1l / 16l, (size_t)fl->precision)) >= 2l)
+	{
+		*val /= 2l;
+		++(*res);
+	}
 	return (i);
 }
 
@@ -30,12 +60,8 @@ int 					ftprt_agetexpon(t_printff *const fl,
 
 	if (val != val || val == flt_inf)
 		return (0);
-	i = normalize_val(&res, &val);
-	while ((val += 0.5l * ft_ldpow(1l / 16l, (size_t)fl->precision)) >= 16l)
-	{
-		val /= 2l;
-		++res;
-	}
+	i = (fl->type == 6) ? normalize_val(fl, &res, &val)
+		: normalize_val_sh(fl, &res, &val);
 	if (n_form)
 		*n_form = val;
 	return ((i == infinite_loop_number) ? 0 : res);
